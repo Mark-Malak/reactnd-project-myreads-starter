bfab5d55f88cd720af1395bcf782b02f35a19cc2
Mark-Malak <markmalak71@gmail.com>
2021-07-20 18:24:25 +0200

    added the website to github pages

---
+++ build/precache-manifest.f928e73e86abd1f5168c7a7d0c8f72a3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "22858661be964832a49c",
+    "url": "/reactnd-project-myreads-starter/static/css/main.e7012b4a.chunk.css"
+  },
+  {
+    "revision": "22858661be964832a49c",
+    "url": "/reactnd-project-myreads-starter/static/js/main.22858661.chunk.js"
+  },
+  {
+    "revision": "87f84a5d270efee94a00",
+    "url": "/reactnd-project-myreads-starter/static/js/1.87f84a5d.chunk.js"
+  },
+  {
+    "revision": "83f7d75ca12013b90cc6",
+    "url": "/reactnd-project-myreads-starter/static/js/runtime~main.83f7d75c.js"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/reactnd-project-myreads-starter/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/reactnd-project-myreads-starter/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/reactnd-project-myreads-starter/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "931ef72b6e6fd437b71a3adaaab76b3d",
+    "url": "/reactnd-project-myreads-starter/static/media/sh.931ef72b.jpg"
+  },
+  {
+    "revision": "651681ecb62eca791fe166c349bf07ad",
+    "url": "/reactnd-project-myreads-starter/index.html"
+  }
+];
